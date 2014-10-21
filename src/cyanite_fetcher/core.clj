2bbf6cc7ea525d89e9f04b3d032f7f288077529d
Kirill Kondratenko <cybem@cybem.info>
2014-10-21 13:17:23 +0400

    Reformat par-fetch-async

--- src/cyanite_fetcher/core.clj
+++ src/cyanite_fetcher/core.clj
@@ -130,17 +130,19 @@
   "Fetch data in parallel fashion using execute-async."
   [session fetch! paths tenant rollup period from to]
   (let [data (atom [])
-        channels (doall (map #(alia/execute-async session fetch!
-                                                  {:values [% tenant (int rollup)
-                                                            (int period)
-                                                            from to]
-                                                   :fetch-size Integer/MAX_VALUE
-                                                   :success (fn [rows]
-                                                              (->> rows
-                                                                   (map detect-aggregate)
-                                                                   (doall)
-                                                                   (seq)
-                                                                   (swap! data conj)))}) paths))]
+        channels
+        (doall (map #(alia/execute-async session fetch!
+                                         {:values [% tenant (int rollup)
+                                                   (int period)
+                                                   from to]
+                                          :fetch-size Integer/MAX_VALUE
+                                          :success (fn [rows]
+                                                     (->> rows
+                                                          (map detect-aggregate)
+                                                          (doall)
+                                                          (seq)
+                                                          (swap! data conj)))})
+                    paths))]
     (map deref channels)
     @data))
 
